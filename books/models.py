54a4c09e4030e6fd61b1b98805b3735af84cdcc6
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-02-09 17:05:13 -0500

    Init models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200)
+
+class Book(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
